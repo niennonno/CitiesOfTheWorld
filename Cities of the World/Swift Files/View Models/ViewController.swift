bd1001cbedc6e16598ef70bcb6ef2d97be11af32
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2018-09-08 16:51:42 +0530

    Simple search logic implemented along with pushing the map view controller.

--- Cities of the World/Swift Files/View Models/ViewController.swift
+++ Cities of the World/Swift Files/View Models/ViewController.swift
@@ -10,19 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //MARK: - Variables
+    
+    var cities = [City]()
+    var filteredCities = [City]()
+    
+    //MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        accessUrl()
+        readFromFile()
     }
+    
+    //MARK: - User Actions
 
-    func accessUrl() {
+    func readFromFile() {
         
         if let url = Bundle.main.url(forResource: "cities", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: url)
-                let json = try JSONSerialization.jsonObject(with: data) as! [[String: Any]]
-                print(json)
+                let jsonArray = try JSONSerialization.jsonObject(with: data) as! [[String: Any]]
+                
+                for json in jsonArray {
+                    cities.append(City(json: json))
+                }
+                cities.sort { $0.cityName < $1.cityName }
+                filteredCities = cities
             }
             catch let error {
                 print(error)
@@ -30,7 +49,48 @@ class ViewController: UIViewController {
         }
         
     }
-
-
 }
 
+
+//MARK: - Table View Data Source and Delegates
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = filteredCities[indexPath.row].cityName + ", " + filteredCities[indexPath.row].countryName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let city = filteredCities[indexPath.row]
+        let mapViewController = storyboard?.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+        mapViewController.cityName = city.cityName
+        mapViewController.latitude = city.latitude
+        mapViewController.longitude = city.longitude
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        self.navigationController?.pushViewController(mapViewController, animated: true)
+    }
+}
+
+//MARK: - Search Bar Delegates
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+                
+        filteredCities = cities.filter({ (city) -> Bool in
+            return city.cityName.hasPrefix(searchText)
+        })
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+    }
+}
